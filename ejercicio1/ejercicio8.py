3339cddd714be615d0d540af60e5be19ed3ce0eb
JavierCasaisFedriani <71722331+JavierCasaisFedriani@users.noreply.github.com>
2021-04-30 21:26:50 +0200

    Delete ejercicio8.py

--- ejercicio1/ejercicio8.py
+++
@@ -1,8 +0,0 @@
-#pide dos numeros y has las 4 operaciones matematicas
-num1 = int(input("Dime el primer numero: "));
-num2 = int(input("Dime el segundo numero: "));
-
-print ("La suma de " + str(num1) + " y " + str(num2) + " es: " + str(num1 + num2));
-print ("La resta de " + str(num1) + " y " + str(num2) + " es: " + str(num1 - num2));
-print ("La multiplicacion de " + str(num1) + " y " + str(num2) + " es: " + str(num1 * num2));
-print ("La divicion de " + str(num1) + " y " + str(num2) + " es: " + str(num1 / num2));
