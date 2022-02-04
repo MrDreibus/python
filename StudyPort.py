dc050fea7caec195a2a010374a7220c6eb81f363
MrDreibus <36729553+MrDreibus@users.noreply.github.com>
2022-02-04 21:05:50 +0100

    Update StudyPort.py

--- StudyPort.py
+++ StudyPort.py
@@ -1,4 +1,3 @@
-
 import random
 
 #FUNCIONES 
@@ -7,15 +6,28 @@ def numPortService(numPor, nameService):
 	solucion = input("Dime el nombre del servicio del puerto " + numPor + ": ")
 	if (solucion.lower() == nameService.lower()):
 		print("Correcto")
+
+	elif (solucion == "exit"):
+		print("Volviendo al menu")
+
 	else:
 		print("Incorrecto, el puerto es " + nameService)
+
+
+	return solucion
 
 def serviceNumPort(numPor, nameService):
 	solucion = input("Dime el puerto servicio de " + nameService + ": ")
 	if (solucion == numPor):
 		print("Correcto")
+
+	elif (solucion == "exit"):
+		print("Volviendo al menu")
+		
 	else:
 		print("Incorrecto, el puerto es " + numPor)
+
+	return solucion
 
 def menu():
 	print ("=========Menu========")
@@ -29,26 +41,39 @@ def menu():
 puertos = ["20-21", "22", "23", "25-587", "53", "80", "110", "137-139", "143", "161-162", "443", "465", "514", "989-990", "993", "995", "3306", "3389", "1194"]
 servicios =["FTP", "SSH", "Telnet", "SMTP", "DNS", "HTTP", "POP3", "NetBios", "IMAP", "SNMP", "HTTPs", "SMTPs", "syslog", "FTPs", "IMAP4s", "POP3s", "MySQL", "RPD", "OpenVPN"]
 
-menu()
+while True:
+	menu()
 
-opc = int(input("Elige una opcion: "))
+	opc = int(input("Elige una opcion: "))
+	cont = 0
 
-if opc == 1:
-	for i in range (0,10,1):
+	if opc == 1:
+		print("Diga 'exit' para salir")
 
-		aleatorio = int(random.randint(0,len(puertos)-1))
-		print(i+1)
+		while True:
+			aleatorio = int(random.randint(0,len(puertos)-1))
+			print(cont + 1)
+			
+			solucion = numPortService(puertos[aleatorio], servicios[aleatorio])
+			cont += 1
+
+			if (solucion == "exit"):
+				break
 		
-		numPortService(puertos[aleatorio], servicios[aleatorio])
-	
-elif opc == 2:
-	for i in range (0,10,1):
+	elif opc == 2:
+		print("Diga 'exit' para salir")
 
-		aleatorio = int(random.randint(0,len(puertos)-1))
-		print(i+1)
-		
-		serviceNumPort(puertos[aleatorio], servicios[aleatorio])
-elif opc == 3:
-	exit()
-else:
-	print("Esta opcion no existe \nSaliendo...")
+		while True:
+			aleatorio = int(random.randint(0,len(puertos)-1))
+			print(cont + 1)
+			
+			solucion = serviceNumPort(puertos[aleatorio], servicios[aleatorio])
+			cont += 1
+
+			if (solucion == "exit"):
+				break
+
+	elif opc == 3:
+		exit()
+	else:
+		print("Esta opcion no existe \nSaliendo...")
